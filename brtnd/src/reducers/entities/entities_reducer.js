af01af749ee9f0aadff9e4146fe1a0a3038e9e1b
tpadgett <tim_padgett@yahoo.com>
2021-04-20 20:51:17 -0700

    reducers v1

--- brtnd/src/reducers/entities/entities_reducer.js
+++ brtnd/src/reducers/entities/entities_reducer.js
@@ -2,13 +2,14 @@ import { combineReducers } from 'redux';
 import orders from './orders/orders_reducer';
 import bartenderReviews from './reviews/bartender_reviews_reducer';
 import userReviews from './reviews/user_reviews_reducer';
-
+import orders from './orders/orders_reducer'
 
 
 const RootReducer = combineReducers({
   orders,
-  bartenderReviews,
-  userReviews
+  bartenderReviews, //same/dif name?
+  userReviews,
+  orders
 });
 
 export default RootReducer;
---
+++ brtnd/src/reducers/entities/reviews/bartender_reviews_reducer.js
@@ -0,0 +1,27 @@
+import {
+  // rename
+  // syntax for bartender/user
+  RECEIVE_BARTENDER_REVIEWS,
+  RECEIVE_BARTENDER_REVIEW,
+  RECEIVE_NEW_BARTENDER_REVIEW
+} from '../actions/bartender_review_actions';
+
+const BartenderReviewsReducer = (state = { all: {}, bartender: {}, new: undefined }, action) => {
+  Object.freeze(state);
+  let newState = Object.assign({}, state);
+  switch (action.type) {
+    case RECEIVE_BARTENDER_REVIEWS:
+      newState.all = action.bartenderReviews.data;
+      return newState;
+    case RECEIVE_BARTENDER_REVIEW:
+      newState.bartender = action.bartenderReviews.data; //check this out on wed -> see how actions are used
+      return newState;
+    case RECEIVE_NEW_BARTENDER_REVIEW:
+      newState.new = action.bartenderReview.data
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default BartenderReviewsReducer;
