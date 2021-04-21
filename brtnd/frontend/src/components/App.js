1b818e5ee5671c693fab50f002c1ab68a2f98c66
Kenneth Schlappkohl <kschlappkohl@gmail.com>
2021-04-21 09:18:31 -1000

    moving folders

--- brtnd/frontend/src/components/App.js
+++ brtnd/frontend/src/components/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { AuthRoute, ProtectedRoute } from "../util/route_util";
+import { Switch } from "react-router-dom";
+
+import Splash from "./splash/splash";
 
 const App = () => {
-  <div>TEST</div>;
+  <div>
+    <Switch>
+      <AuthRoute exact path="/" component={Splash} />
+    </Switch>
+  </div>;
 };
 
 export default App;
