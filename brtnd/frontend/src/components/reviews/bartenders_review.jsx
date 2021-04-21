2903ab53c764c495587fa298bda7de5bb67389cd
tpadgett <tim_padgett@yahoo.com>
2021-04-21 11:13:09 -0700

    component structure setup

---
+++ brtnd/frontend/src/components/reviews/bartenders_review.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from '../navbar/navbar';
+import { Link } from 'react-router-dom';
+
+const BartenderReview = (props) => {
+  return (
+    null
+  )
+};
+
+export default BartenderReview;
---
+++ brtnd/frontend/src/components/splash/splash.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import React from 'react';
+import Navbar from '../navbar/navbar';
+import { Link } from 'react-router-dom';
+
+const Splash = (props) => {
+  return (
+    null
+  )
+};
+
+export default Splash;
