62f129c12bc24e68140cdf14576c33fbaa7f2b4d
tpadgett <tim_padgett@yahoo.com>
2021-04-21 12:21:26 -0700

    restructure files

--- brtnd/frontend/src/components/navbar/navbar.jsx
+++ brtnd/frontend/src/components/navbar/navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from '../navbar/navbar';
+import Navbar from './navbar';
 import { Link } from 'react-router-dom';
 
 const BartenderSignupForm = (props) => {
