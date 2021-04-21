f1e8536c3c10e756a6e879f14cfe38b11cc16d8a
tpadgett <tim_padgett@yahoo.com>
2021-04-21 11:31:32 -0700

    build splash

--- brtnd/frontend/src/components/order/order_form.jsx
+++ brtnd/frontend/src/components/order/order_form.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Navbar from '../navbar/navbar';
 import { Link } from 'react-router-dom';
+import './order.css'
 
 const OrderForm = (props) => {
   return (
-    null
+    
+    
+    
   )
 };
 
