59bbee900723607139e062c917056f3a44b714f0
Kenneth Schlappkohl <kschlappkohl@gmail.com>
2021-04-22 08:30:50 -1000

    New folder restructure

--- brtnd/frontend/src/components/App.js
+++ brtnd/frontend/src/components/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import { Switch } from "react-router-dom";
 
-import Splash from "./splash/splash";
-import HostSessionContainer from "./session/host_session_container";
+import Splash from "./splash/Splash";
+import HostSessionContainer from "../components/session/host_session/host_session_container";
 
 
 import "./App.css";
