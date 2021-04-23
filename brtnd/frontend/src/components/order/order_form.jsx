53c0a66ccf5a1115763684b68073f806a52218a7
tpadgett <tim_padgett@yahoo.com>
2021-04-22 18:55:18 -0700

    orders

--- brtnd/frontend/src/components/order/order_form.jsx
+++ brtnd/frontend/src/components/order/order_form.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Navbar from '../navbar/navbar';
 import { Link } from 'react-router-dom';
 import OrderErrors from "./order_errors";
@@ -6,21 +6,21 @@ import OrderForm from "./order_form_container"
 import './order.css'
 import order from '../../../../validation/order';
 
-const OrderForm = ({ 
-  createOrder, 
+const OrderForm = ({
+  createOrder,
   clearOrderErrors,
   editOrder,
   errors
 }) => {
 
   const [newOrder, setNewOrder] = useState({
-      headCount: "",
-      tier: "",
-      location: "",
-      notes: "",
-      price: 0,
-      orderedBy: "",
-      takenBy: "",
+    headCount: "",
+    tier: "",
+    location: "",
+    notes: "",
+    price: 0,
+    orderedBy: "",
+    takenBy: "",
   })
 
   const handleSubmit = (e) => {
@@ -37,63 +37,48 @@ const OrderForm = ({
   };
 
   const handleChange = (e) => {
-    const {name, value} = e.target;
+    const { name, value } = e.target;
 
     setNewOrder((order) => ({
       ...order,
-        [name]: value,
+      [name]: value,
     }));
-
-
   }
-
-
-
 
   return (
 
+    <div className='order-form-parent-container'>
 
-  
-  <div className='order-form-parent-container'>
-      {/* <head className='order-form-parent-header'>
-          <link rel="stylesheet" href="order.css"/>
-          <link rel="preconnect" href="https://fonts.gstatic.com"/>
-          <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap" rel="stylesheet"/>
-      </head> */}
-      
-      <body className='order-form-main'>
+      <div className='order-form-main'>
         <div class="order-header">
           <h1>Order a bartender</h1>
         </div>
-
+        
         <form class="order-form">
           <div class="order-form-headcount">
             <label class="select">Select your headcount</label>
-              <div class="quantity">
-                <input type="number" id="quantity" name="quantity" min="1" max="20"/>
-              </div>
+            <div class="quantity">
+              <input type="number" id="quantity" name="quantity" min="1" max="20" onChange={handleChange} />
+            </div>
           </div>
-            <label class="select">Select a tier package</label>
-                <div class="radio-tier">
-                  <input type="radio" id="tier-one" name="tier" value="Tier one" checked={newOrder.tier === "Tier one"} onChange={handleChange}/>
-                  <label for="tier-one">Tier one</label>
-                  <input type="radio" id="tier-two" name="tier" value="Tier two" checked={newOrder.tier === "Tier two"} onChange={handleChange}/>
-                  <label for="tier-two">Tier two</label>
-                  <input type="radio" id="tier-three" name="tier" value="Tier three" checked={newOrder.tier === "Tier three"} onChange={handleChange}/>
-                  <label for="tier-three">Tier three</label>
-                </div>
-              
-            <label class="select">Tell us about your event</label> 
-                <input class="location" type="text" id="location" name="location" placeholder="location"/>
-                <textarea class="order-notes"  rows="4" cols="50"  id="notes" name="notes" placeholder="Any notes?"></textarea>
-                <input class="sub" type="submit" value="Place Order"/>
-        </form>
+          <label class="select">Select a tier package</label>
+          <div class="radio-tier">
+            <input type="radio" id="tier-one" name="tier" value="Tier one" checked={newOrder.tier === "Tier one"} onChange={handleChange} />
+            <label for="tier-one">Tier one</label>
+            <input type="radio" id="tier-two" name="tier" value="Tier two" checked={newOrder.tier === "Tier two"} onChange={handleChange} />
+            <label for="tier-two">Tier two</label>
+            <input type="radio" id="tier-three" name="tier" value="Tier three" checked={newOrder.tier === "Tier three"} onChange={handleChange} />
+            <label for="tier-three">Tier three</label>
+          </div>
 
-        {/* <div class="details">
-        </div> */}
-      </body>
-  </div>
-    
+          <label class="select">Tell us about your event</label>
+          <input class="location" type="text" id="location" name="location" placeholder="Location" onChange={handleChange} />
+          <textarea class="order-notes" rows="4" cols="50" id="notes" name="notes" placeholder="Any notes?" onChange={handleChange}></textarea>
+          <input class="sub" type="submit" value="Place Order" onChange={handleSubmit} />
+        </form>
+      </div>
+    </div>
+
   );
 
 
