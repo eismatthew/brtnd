46549d0eff732ba6c78944f19d338fdbbcea3091
tpadgett <tim_padgett@yahoo.com>
2021-04-25 21:19:25 -0700

    finish review form v1

--- brtnd/frontend/src/components/reviews/bartenders_review.jsx
+++ brtnd/frontend/src/components/reviews/bartenders_review.jsx
@@ -4,34 +4,34 @@ import { Link } from 'react-router-dom';
 import "./bartender-review-form.css";
 
 const BartenderReview = ({ createReview }) => {
-
-  const [reviewedBy, setReviewedby] = useState(null);
-  const [reviewedFor, setReviewedFor] = useState(null);
+  
+  // const [reviewedBy, setReviewedby] = useState(null);
+  // const [reviewedFor, setReviewedFor] = useState(null);
   const [newReview, setNewReview] = useState({
-    reviewer: reviewedBy,
+    reviewer: "",
     reviewee: "",
     rating: "",
     comments: ""
   });
-  
-  useEffect(() => {
-    let config = {
-      headers: {
-        Authorization: localStorage.jwtToken,
-      },
-    };
-    axios
-      .get("/api/users/current/user", config)
-      .then((res) => setReviewedBy(res.data.id))
-      .then(() => setNewReview((review) => ({ ...review, reviewedBy: reviewedBy })));
-  }, [reviewedBy]);
+  console.log(newReview);
+  // useEffect(() => {
+  //   let config = {
+  //     headers: {
+  //       Authorization: localStorage.jwtToken,
+  //     },
+  //   };
+  //   axios
+  //     .get("/api/users/current/user", config)
+  //     .then((res) => setReviewedBy(res.data.id))
+  //     .then(() => setNewReview((review) => ({ ...review, reviewedBy: reviewedBy })));
+  // }, [reviewedBy]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(newReview);
-    createReview(newReview);
+    // console.log(newReview);
+    // createReview(newReview);
     setNewReview({
-      reviewer: reviewedBy,
+      reviewer: "",
       reviewee: "",
       rating: "",
       comments: ""
@@ -50,61 +50,63 @@ const BartenderReview = ({ createReview }) => {
   <div className="review-main">
       <div className="review-container">
         <h1 className="rating-header">Rate your experience</h1>
-        <form className="review" onSubmit={handleSubmit}>
+        <form className="review" 
+        onSubmit={handleSubmit}
+        >
           <input 
           className="bartender-name" 
           type="text"
           placeholder="Bartender name" 
-          name="bartender-name"
+          name="reviewee"
           value={newReview.reviewee}
           onChange={handleChange}
           />
           <div className="radio-box">
             <input 
             type="radio" 
-            name="radioRating" 
+            name="rating" 
             id="one" 
             value="1"
             checked={newReview.rating === 1}
             onChange={handleChange}
             />
-            <label className="rating" for="one">1</label>
+            <label className="rating" htmlFor="one">1</label>
             <input 
             type="radio" 
-            name="radioRating" 
+              name="rating"
             id="two" 
             value="2"
             checked={newReview.rating === 2}
             onChange={handleChange}
             />
-            <label className="rating" for="two">2</label>
+            <label className="rating" htmlFor="two">2</label>
             <input 
             type="radio" 
-            name="radioRating" 
+              name="rating"
             id="three" 
             value="3"
             checked={newReview.rating === 3}
             onChange={handleChange}
             />
-            <label className="rating" for="three">3</label>
+            <label className="rating" htmlFor="three">3</label>
             <input 
             type="radio" 
-            name="radioRating" 
+              name="rating"
             id="four" 
             value="4"
             checked={newReview.rating === 4}
             onChange={handleChange}
             />
-            <label className="rating" for="four">4</label>
+            <label className="rating" htmlFor="four">4</label>
             <input 
             type="radio" 
-            name="radioRating" 
+              name="rating"
             id="five" 
             value="5"
-            checked={newReview.rating === 1}
+            checked={newReview.rating === 5}
             onChange={handleChange}
             />
-            <label className="rating" for="five">5</label>
+            <label className="rating" htmlFor="five">5</label>
           </div>
           <br/>
             <textarea 
@@ -112,7 +114,8 @@ const BartenderReview = ({ createReview }) => {
             placeholder="Comments" 
             rows="4" 
             cols="50"
-            value={newOrder.comments}
+            name="comments"
+            value={newReview.comments}
             onChange={handleChange}
             ></textarea>
             <br/>
